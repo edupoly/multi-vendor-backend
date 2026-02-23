20581aa5b8be5a33431ac92681c06b76ae0cf115
edupoly <info.edupoly@gmail.com>
2026-02-23 21:00:43 +0530

    products-by-vendor

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,19 +1,20 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
   createProduct,
   getProducts,
   getProductById,
   updateProduct,
   deleteProduct,
-} from '../controllers/product.controller';
-import { auth, isVendor } from '../middleware/auth.middleware';
+} from "../controllers/product.controller";
+import { auth, isVendor } from "../middleware/auth.middleware";
 
 const router = Router();
 
-router.post('/', [auth, isVendor], createProduct);
-router.get('/', getProducts);
-router.get('/:id', getProductById);
-router.put('/:id', [auth, isVendor], updateProduct);
-router.delete('/:id', [auth, isVendor], deleteProduct);
+router.post("/", [auth, isVendor], createProduct);
+router.get("/", getProducts);
+router.get("/:id", getProductById);
+router.get("/vendorProducts/:id", getProductsByVendorId);
+router.put("/:id", [auth, isVendor], updateProduct);
+router.delete("/:id", [auth, isVendor], deleteProduct);
 
 export default router;
