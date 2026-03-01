75b5bcfe44e788e9000f0764f8937570ad961f94
edupoly <info.edupoly@gmail.com>
2026-03-01 19:50:33 +0530

    userid-login-response

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -79,6 +79,7 @@ export const login = async (req: Request, res: Response) => {
           role: user.role,
           name: user.name,
           email: user.email,
+          id: user.id,
         });
       },
     );
